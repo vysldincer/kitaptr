258a9fb8158f484cf03e977bc757c0d4b0c561e9
vysldincer <vysldincer@gmail.com>
2021-11-24 22:29:11 +0300

    mobil düzenlemeler yapıldı

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import './App.css';
 function App() {
   return (
     <div className="App  ">
-      <div className="container mx-auto h-auto ">
+      <div className="container p-4 sm:p-0 sm:mx-auto  h-auto  ">
 
         <div className="bg-gray-100 rounded-xl p-8 mt-3 text-center text-4xl font-bold">
           <h1>Mekki - Medeni Analiz</h1>
         </div>
 
-        <div className="flex bg-gray-100 divide-x  divide-blue-400 text-center  rounded-xl p-8 mt-3">
+        <div className="md:flex   bg-gray-100     md:divide-x  divide-blue-400 text-center  rounded-xl p-8 mt-3">
 
-          <div className=" flex-grow  p-3  ">
+          <div className=" md:flex-grow  p-3  ">
             <input type="text" className="w-full h-10 p-3 rounded-sm  border" placeholder="Kelime Ara" />
           </div>
 
@@ -29,22 +29,22 @@ function App() {
           <div className="  h-12 mb-2 rounded-md flex items-center justify-center text-blue-500 text-2xl font-extrabold">BULUNAN SONUÇLAR</div>
 
 
-          <div className="grid grid-cols-4 gap-4">
+          <div className="grid  grid-cols-1 gap-2 lg:grid-cols-4 lg:gap-4">
 
-            <div className="bg-blue-200 h-10 rounded flex items-center justify-center text-blue-700 text-2xl ">
-              Âyet Sayısı : <div className="bg-blue-500 border text-white leading-10 mx-3 px-2 h-full ">1</div>
+            <div className="bg-blue-200 pl-4 h-10 rounded flex items-center justify-between  text-blue-700 text-2xl ">
+              Âyet Sayısı : <div className="bg-blue-500 border text-white leading-10 px-2 h-full ">1</div>
             </div>
 
-            <div className="bg-blue-200 h-10 rounded flex items-center justify-center text-blue-700 text-2xl ">
-              Mekki Âyet Sayısı : <div className="bg-blue-500 border text-white leading-10 mx-3 px-2 h-full ">1</div>
+            <div className="bg-blue-200 pl-4 h-10 rounded flex items-center justify-between  text-blue-700 text-2xl ">
+              Mekki Âyet Sayısı : <div className="bg-blue-500 border text-white leading-10 px-2 h-full ">1</div>
             </div>
 
-            <div className="bg-blue-200 h-10 rounded flex items-center justify-center text-blue-700 text-2xl ">
-              Medeni Âyet Sayısı : <div className="bg-blue-500 border text-white leading-10 mx-3 px-2 h-full ">1</div>
+            <div className="bg-blue-200 pl-4 h-10 rounded flex items-center justify-between  text-blue-700 text-2xl ">
+              Medeni Âyet Sayısı : <div className="bg-blue-500 border text-white leading-10 px-2 h-full ">1</div>
             </div>
 
-            <div className="bg-blue-200 h-10 rounded flex items-center justify-center text-blue-700 text-2xl ">
-              Toplam Sonuç : <div className="bg-blue-500 border text-white leading-10 mx-3 px-2 h-full ">1</div>
+            <div className="bg-blue-200 pl-4 h-10 rounded flex items-center justify-between  text-blue-700 text-2xl ">
+              Toplam Sonuç : <div className="bg-blue-500 border text-white leading-10 px-2 h-full ">1</div>
             </div>
 
           </div>
